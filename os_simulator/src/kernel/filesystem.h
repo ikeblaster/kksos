3aca456935bc787d11fe2ac78c1b5c4632faac01
Matěj Kareš <karesmatej93@gmail.com>
2016-10-25 12:36:43 +0200

    Filesystem function decalarations

--- os_simulator/src/kernel/filesystem.h
+++ os_simulator/src/kernel/filesystem.h
@@ -19,3 +19,36 @@ typedef struct fsfile : fsnode {
 typedef struct fsdir : fsnode {
 	std::map<std::string, struct fsnode*> children;
 };
+
+/* Vytvori soubor v adresari "parent" se jmenem "name" a obsahem "data" */
+void create_file(fsdir *parent, std::string name, std::string data);
+
+
+/* Vytvori v adresari "parent" novy adresar se jmenem "name" */
+void create_dir(fsdir *parent, std::string name);
+
+
+/* Smaze soubor "name" v adresari "parent" */
+void delete_file(fsdir *parent, std::string name);
+
+
+/* Smaze adresar "name" v adresari "parent" */
+void delete_dir(fsdir *parent, std::string name);
+
+//void open_file(fsdir *parent, std::string name);
+
+
+/* Vrati obsah souboru "name" v adresari "parent" */
+std::string read_file(fsdir *parent, std::string name);
+
+
+/* Vrati predka uzlu "node" */
+fsdir get_parent(fsnode *node);
+
+
+/* Zkopiruje soubor "name" z adresare "from" do adresare "to" */
+void copy_file(fsdir *from, fsdir *to, std::string name);
+
+
+/* Premisti soubor "name" z adresare "from" do adresare "to" */
+void move_file(fsdir *from, fsdir *to, std::string name);

a5f49b589d1c83ca7f3e6f0319dd496871a1b36f
Matěj Kareš <karesmatej93@gmail.com>
2016-11-15 23:02:01 +0100

    Forgot one file :dancer:

--- os_simulator/src/user/shell.h
+++ os_simulator/src/user/shell.h
@@ -2,8 +2,6 @@
 #include "..\common\api.h"
 #include "rtl.h"
 #include "parser.h"
-#include "sort.h"
-#include "wc.h"
 #include "vmstdio.h"
 
 extern "C" size_t __stdcall shell(const CONTEXT &regs);

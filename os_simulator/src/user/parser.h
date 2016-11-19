9f1529928e8a52b83f8204d966eca688b0c1dda1
ikeblaster <ike@thez.info>
2016-11-19 11:35:33 +0100

    Parser.h - code formatting unification

--- os_simulator/src/user/parser.h
+++ os_simulator/src/user/parser.h
@@ -12,17 +12,17 @@
 struct Command {
 	std::string name;
 	std::vector<char> params;
+	std::vector<std::string> data;
 	std::string redirectStdin;
 	std::string redirectStdout;
 	std::string redirectAStdout;
-	std::vector<std::string> data;
 };
 
 /// <summary>
 /// Class for parsing data from command line
 /// </summary>
 class parser {
-public:
+	public:
 	/// <summary>
 	/// Saves data into command struct
 	/// </summary>
@@ -40,27 +40,27 @@ public:
 	/// <returns>True if parsing ok, false if not</returns>
 	bool parse(std::string input);
 
-	std::queue<Command> commandList; /* Vector of commands */
+	std::queue<Command> commandList; // Vector of commands
 
-private:
-	/* Special symbols */
-	static const char PIPE = '|'; /* pipe */
-	static const char STDIN = '<'; /* stdin redirect */
-	static const char STDOUT = '>'; /* stdout redirect */
-	static const char PARAMPREFIX = '/'; /* param prefix */
-	static const char QUOTE = '"'; /* start of string */
-	static const char SPACE = ' '; /* space */
+	private:
+	// Special symbols
+	static const char PIPE = '|'; // pipe
+	static const char STDIN = '<'; // stdin redirect
+	static const char STDOUT = '>'; // stdout redirect
+	static const char PARAMPREFIX = '/'; // param prefix
+	static const char QUOTE = '"'; // start of string
+	static const char SPACE = ' '; // space
 
-	/* Temp bools */
-	bool commandOK = false; /* Command is allowed */
-	bool pipe = false; /* If pipe hasn't followed command */
-	bool getParam = false; /* Next char is param */
-	bool out = false; /* Temp Astdout or stdout */
-	bool getStdin = false; /* Next is stdin */
-	bool getAStdout = false; /* Next is append stdout */
-	bool getStdout = false; /* Next is stdout */
-	bool openQuote = false; /* Is processing line in quote */
-	std::string temp = ""; /* Actual processing string */
-	int i; /* Actual position in input */
-	size_t last; /* Position of last char in input */
+								   // Temp bools
+	bool commandOK = false; // Command is allowed
+	bool pipe = false; // If pipe hasn't followed command
+	bool getParam = false; // Next char is param
+	bool out = false; // Temp Astdout or stdout
+	bool getStdin = false; // Next is stdin
+	bool getAStdout = false; // Next is append stdout
+	bool getStdout = false; // Next is stdout
+	bool openQuote = false; // Is processing line in quote
+	std::string temp = ""; // Actual processing string
+	int i; // Actual position in input
+	size_t last; // Position of last char in input
 };
